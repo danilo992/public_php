162ac379c2cbb123b7d338d4b0b45f7b9c9c95fb
danilo992 <tadeudanilo46@gmail.com>
2019-09-27 18:18:44 -0300

    refazendo a section de planos e atualizando para codigos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-function toggleClass() {
-    let menu = document.querySelector("#mainMenu");
-    menu.classList.toggle("toggleCls");
+function funcao1() {
+alert("Econômico\nR$89,90\n(Todos os dias\n das 11:30-14:00)\n(48)98833-8355");
 }
